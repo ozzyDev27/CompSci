a789f7956355ff92fa63bca131933efedba8a87f
ozzy <businessozzy27@gmail.com>
2026-03-26 23:37:49 -0400

    a

--- POTDs/CH9/src/Front11.java
+++ POTDs/CH9/src/Front11.java
@@ -1,8 +1,7 @@
 public class Front11 {
     public static int[] front11(int[] a, int[] b)
 	{
-		int size = (a.length > 0 ? 1 : 0) + (b.length > 0 ? 1 : 0);
-		int[] c = new int[size];
+		int[] c = new int[(a.length > 0 ? 1 : 0) + (b.length > 0 ? 1 : 0)];
 		int d = 0;
 
 		if (a.length > 0) c[d++] = a[0];
@@ -15,5 +14,4 @@ public class Front11 {
 	{
 		System.out.println();
 	}
-
 }
