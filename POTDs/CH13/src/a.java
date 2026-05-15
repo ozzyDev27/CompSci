22e14a7c497341d39b23fad75069dc010dca759b
ozzy <businessozzy27@gmail.com>
2026-05-14 22:54:03 -0400

    Create a.java

---
+++ POTDs/CH13/src/a.java
@@ -0,0 +1,29 @@
+public class a {
+public static void main(String[] args) {
+boolean a=true;
+boolean b=true;
+boolean c=true;
+String name = "Jamie";
+int number = 3;
+
+//Comment for entire switch
+switch(number) {
+    case 0: 
+        //Comment for case
+        System.out.println();
+        break;
+    case 1:
+        //Comment for case
+        System.out.println();
+        break;
+    default:
+        //if nothing else, will run this
+        break;
+}
+
+
+
+
+
+}
+}
